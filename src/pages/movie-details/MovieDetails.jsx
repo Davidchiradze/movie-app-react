378ca269c10d1fe81a3612514c9ff53035fb532b
David <david@unlimited-robotics.com>
2022-12-21 14:51:57 +0400

    add images to movies

--- src/pages/movie-details/MovieDetails.jsx
+++ src/pages/movie-details/MovieDetails.jsx
@@ -1,6 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MoviesDataContext } from "../../contexts/moviesData";
+import {
+  DetailsWrapper,
+  MovieDetailsPageWrapper,
+  MovieImage,
+} from "./components";
 
 const MovieDetails = () => {
   const [chosenMovie, setChosenMovie] = useState({});
@@ -11,7 +16,19 @@ const MovieDetails = () => {
     setChosenMovie(moviesData.find((movie) => movie?.id?.toString() === id));
   }, [id, moviesData]);
 
-  return <div>{chosenMovie.name}</div>;
+  return (
+    <MovieDetailsPageWrapper>
+      <DetailsWrapper>
+        <div>
+          <MovieImage src={chosenMovie.img} />
+        </div>
+        <div>
+          <h2>{chosenMovie.name}</h2>
+          <h3>{chosenMovie.genre}</h3>
+        </div>
+      </DetailsWrapper>
+    </MovieDetailsPageWrapper>
+  );
 };
 
 export default MovieDetails;
---
+++ src/pages/movie-details/components.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MovieImage = styled.img`
+  width: 250px;
+`;
+
+export const MovieDetailsPageWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const DetailsWrapper = styled.div`
+  display: flex;
+  margin-top: 20px;
+  gap: 20px;
+`;
